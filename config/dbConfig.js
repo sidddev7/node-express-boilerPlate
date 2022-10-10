db6e4ba582c039ace243177d9f631382d9ec4186
Siddhrajsinh Ranjitsinh Gohil <61483081+sidddev7@users.noreply.github.com>
2022-10-10 22:21:34 +0530

    Update dbConfig.js

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -4,6 +4,3 @@ const mongo = {
   mongoUrlLocal: "YOUR_LOCAL_URL_HERE",
 };
 export default mongo;
-
-
-// Add the mongo URLs in the en file so that it can imported as per the environment you select
